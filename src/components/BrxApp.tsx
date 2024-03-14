7c9306298781e8970ae01fe38a7821f10a077632
Dmytro Hachok <dmytro.hachok@bloomreach.com>
2024-03-14 14:55:48 +0100

    Update state inside BrxApp without initialization

--- src/components/BrxApp.tsx
+++ src/components/BrxApp.tsx
@@ -14,55 +14,62 @@
  * limitations under the License.
  */
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrComponent, BrPage, BrPageContext } from '@bloomreach/react-sdk';
 import { Link, useLocation } from 'react-router-dom';
+import { Configuration } from '@bloomreach/spa-sdk';
 import axios from 'axios';
 import { Menu } from './Menu';
-import { buildConfiguration } from '../utils/buildConfiguration';
 import { Banner } from './Banner';
 import { Content } from './Content';
 import { NewsList } from './NewsList';
+import { buildConfiguration } from '../utils/buildConfiguration';
 
-export const BrxApp: React.FC = React.memo(() => {
+export const BrxApp: React.FC = () => {
   const location = useLocation();
-  const counter2 = useRef(0);
 
-  const configuration = buildConfiguration(`${location.pathname}${location.search}`, axios);
+  const [value, setValue] = useState('');
+  const [configuration, setConfiguration] = useState<Configuration | undefined>();
+
   const mapping = { Banner, Content, 'News List': NewsList, 'Simple Content': Content };
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      counter2.current = 2;
-    }, 2000);
+      setValue('Value is set');
+    }, 4000);
 
     return () => {
       clearTimeout(timeoutId);
     };
   }, []);
 
+  useEffect(() => {
+    setConfiguration(buildConfiguration(`${location.pathname}${location.search}`, axios));
+  }, [location]);
+
   return (
     <div style={{ border: '1px solid red' }}>
       Child component
 
+      {!!configuration && (
       <BrPage configuration={configuration} mapping={mapping}>
         <header>
           <nav className="navbar navbar-expand-sm navbar-dark sticky-top bg-dark" role="navigation">
 
             <div className="bg-light">
-              Counter2:
+              Value:
               {' '}
-              { counter2.current }
+              {value}
             </div>
 
             <div className="container">
               <BrPageContext.Consumer>
                 {(page) => page
-                      && (
-                      <Link to={page.getUrl('/')} className="navbar-brand">
-                        {page.getTitle() || 'brXM + React = ♥️'}
-                      </Link>
-                      )}
+                                        && (
+                                        <Link to={page.getUrl('/')} className="navbar-brand">
+                                          {page.getTitle() || 'brXM + React = ♥️'}
+                                        </Link>
+                                        )}
               </BrPageContext.Consumer>
               <div className="collapse navbar-collapse">
                 <BrComponent path="menu">
@@ -84,6 +91,7 @@ export const BrxApp: React.FC = React.memo(() => {
           </div>
         </footer>
       </BrPage>
+      )}
     </div>
   );
-});
+};
